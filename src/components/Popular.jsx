c900a5060b92a2c880e8c3ffd3b45c243cc3d08e
dosha10 <ndungufrank01@gmail.com>
2022-07-08 23:35:57 +0300

    env

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,10 @@
 function Popular() {
-  return (
-    <div>Popular</div>
-  )
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}`
+    );
+  };
+  return <div>Popular</div>;
 }
 
-export default Popular
+export default Popular;
