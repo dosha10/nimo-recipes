a886645229904a3697a099fe15e4c3b3c92a0458
dosha10 <ndungufrank01@gmail.com>
2022-07-08 23:33:05 +0300

    initial commit

---
+++ src/components/Veggies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Veggies() {
+  return <div>Veggies</div>;
+}
+
+export default Veggies;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Veggie from "../components/Veggies";
+import Popular from "../components/Popular";
+
+function Home() {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
